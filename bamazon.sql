8d9f71a8f96dadf4233aea7ef3de45f54d52a585
Blonded <Lena@lenas-mbp.attlocal.net>
2018-05-29 20:15:41 -0700

    added my sql table

---
+++ bamazon.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS bamazon_DB;
+CREATE DATABASE bamazon_DB;
+
+USE bamazon_DB;
+
+CREATE TABLE products(
+item_id INT NOT NULL AUTO_INCREMENT,
+product_name VARCHAR(50) NULL,
+department_name VARCHAR (50) NULL,
+price FLOAT,
+stock_quantity INT
+PRIMARY KEY (item_id),
+);
