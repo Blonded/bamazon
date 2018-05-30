91170d4f88044b8811fade3ad64a49b8950f5eab
Blonded <Lena@lenas-mbp.attlocal.net>
2018-05-29 23:44:37 -0700

    adding psuedocode

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -1,7 +1,11 @@
+
+// NPM install packages
+// ALL packages are REQUIRED for this app to function properly.
 var mysql = require ("mysql");
 var inquirer = require("inquirer");
 var table = require("cli-table2");
 
+// creating connection with mysql, via local host
 var connection = mysql.createConnection({
 	host:"localhost",
 	port: 3306,
@@ -17,18 +21,24 @@ connection.connect(function (err) {
   // console.log('We're connected!');
 });
 
-
+// start function: to prompt the user where they want to go, and what options
+// they have available to them.
 function start(){
   console.log("\n --------------------------- \n");
+  // creating a connection to the sql documents products table, pulling data
   connection.query("SELECT FROM * products", function (err,response) {
+    // if there is an error then it will throw an error,
+    // if not it will log the response.
     if (err) throw err;
       // console.log(response);
+
+      // adding the products into an array so they are easier to select from
     var productId = [];
     for (var i = 0; i < response.length; i++){
       productId.push(response[i]["item_id"]);
     };
   })
-
+// prompt the user with the navigated question
   inquirer
   .prompt ([
     {
@@ -38,19 +48,24 @@ function start(){
     }
   ])
 
+}
 
-  // The app should then prompt users with two messages.
-  // The first should ask them the ID of the product they would like to buy.
-  // The second message should ask how many units of the product they would like to buy.
+// The first should ask them the ID of the product they would like to buy.
+// The second message should ask how many units of the product they would like to buy.
 
+function productPurchase(){
+// What is the id of the product you would like to purchase?
+}
+
+function productConsumerQuantity(){
+  // How many units of this product would you like to buy?
 }
 
 
 
 
 
-
-function quitApp() {
+function quit() {
     console.log("\n --------------------------------- \n");
     console.log("Thanks for using *Bamazon* !");
     console.log("\n --------------------------------- \n");
